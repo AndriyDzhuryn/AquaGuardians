4c24288a87403d8d207929a1277a28190cfa9d9e
Andriy Dzhuryn <165391426+AndriyDzhuryn@users.noreply.github.com>
2025-01-31 01:49:50 +0200

    init project

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,71 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const authInstance = axios.create({
+  baseURL: 'https://connections-api.goit.global/',
+});
+
+export const setToken = token => {
+  authInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+export const clearToken = () => {
+  authInstance.defaults.headers.common.Authorization = '';
+};
+
+export const register = createAsyncThunk(
+  'auth/register',
+  async (formData, thunkAPI) => {
+    try {
+      const { data } = await authInstance.post('/users/signup', formData);
+      setToken(data.token);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (formData, thunkAPI) => {
+    try {
+      const { data } = await authInstance.post('/users/login', formData);
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logout = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    const state = thunkAPI.getState();
+    const token = state.auth.token;
+    setToken(token);
+    await authInstance.post('/users/logout');
+    clearToken();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+export const refreshUser = createAsyncThunk(
+  'auth/refreshUser',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const token = state.auth.token;
+
+    if (token === null) {
+      return thunkAPI.rejectWithValue('No token provided to refresh user');
+    }
+
+    try {
+      setToken(token);
+      const { data } = await authInstance.get('/users/current');
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
---
+++ src/redux/store.js
@@ -0,0 +1,34 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  persistStore,
+  persistReducer,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import { authReducers } from "./auth/slice.js";
+
+const authConfig = {
+  key: "auth",
+  storage: storage,
+  whitelist: ["token"],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authConfig, authReducers),
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
