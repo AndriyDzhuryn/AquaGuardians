b318b6164810fbad2e17604f0d785f1bd16c21d6
Anastasia <tildatopdev@gmail.com>
2025-02-02 11:49:56 +0200

    add delete btn

--- src/components/App/deleteEntry/DeleteEntry.jsx
+++ src/components/App/deleteEntry/DeleteEntry.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal';
 import css from './deleteEntry.module.css';
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 Modal.setAppElement('#root');
 
-const DeleteEntry = () => {
-  const [modalIsOpen, setIsOpen] = React.useState(false);
+const DeleteEntry = ({ entryId, setEntries }) => {
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   const openModal = () => setIsOpen(true);
   const closeModal = () => setIsOpen(false);
+
+  const handleDelete = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      await axios.delete(`https://your-api.com/api/water-intake/${entryId}`, {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      setEntries(prevEntries =>
+        prevEntries.filter(entry => entry._id !== entryId)
+      ); //setEntries - стан у компоненті listWater
+
+      setIsOpen(false);
+    } catch (error) {
+      iziToast.error({ title: 'Error', message: 'Failed to delete entry' });
+    }
+  };
 
   return (
     <div>
@@ -25,13 +45,26 @@ const DeleteEntry = () => {
             <h3>Delete entry</h3>
             <p>Are you sure you want to delete the entry?</p>
           </div>
-          <button>
-            <img src="../../../../public/icons/icons-sprite.svg#close-modal" />
+          <button type="button" onClick={closeModal}>
+            <svg width="24" height="24" viewBox="0 0 24 24">
+              <path
+                stroke="#407bff"
+                fill="blue"
+                d="M6 18L18 6M6 6L18 18"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
           </button>
         </div>
         <div className={css.control}>
-          <button className={css.btnCancel}>Cancel</button>
-          <button className={css.btnDelete}>Delete</button>
+          <button className={css.btnCancel} type="button" onClick={closeModal}>
+            Cancel
+          </button>
+          <button className={css.btnDelete} onClick={handleDelete}>
+            Delete
+          </button>
         </div>
       </Modal>
     </div>
