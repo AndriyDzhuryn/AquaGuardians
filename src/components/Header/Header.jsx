8707aa8f14ac7835eb6c1fb660e02d9882369add
kmositua <kmositua@gmail.com>
2025-02-10 09:10:01 +0000

    fix UserLogoutModal name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,12 +5,6 @@ import UserLogo from '../UserLogo/UserLogo.jsx';
 import css from './Header.module.css';
 import { selectAuthIsLoggedIn } from '../../redux/auth/selectors.js';
 
-// const user = {
-//   name: '',
-//   email: 'david@gmail.com',
-//   avatar: '../../../public/logo.svg',
-// };
-
 const Header = () => {
   const isLoggedIn = useSelector(selectAuthIsLoggedIn);
 
