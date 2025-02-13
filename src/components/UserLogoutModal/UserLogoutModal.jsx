d01202f1ca7a21bc5257fa2f2da3fc01ba9df242
Andriy Dzhuryn <165391426+AndriyDzhuryn@users.noreply.github.com>
2025-02-13 15:42:12 +0200

    fix loader and btnlogout

--- src/components/UserLogoutModal/UserLogoutModal.jsx
+++ src/components/UserLogoutModal/UserLogoutModal.jsx
@@ -1,10 +1,14 @@
 import css from '../UserLogoutModal/UserLogoutModal.module.css';
 import { apiLogOutUser } from '../../redux/auth/operations.js';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import iziToast from 'izitoast';
+import { selectAuthIsLoading } from '../../redux/auth/selectors.js';
+import { useEffect } from 'react';
 
 const UserLogoutModal = ({ onClose }) => {
   const dispatch = useDispatch();
+
+  const logoutLoading = useSelector(selectAuthIsLoading);
 
   const handleLogOut = async e => {
     e.preventDefault();
@@ -17,20 +21,33 @@ const UserLogoutModal = ({ onClose }) => {
     }
   };
 
+  useEffect(() => {
+    if (logoutLoading) {
+      onClose();
+    }
+  }, [logoutLoading, onClose]);
+
   return (
     <div className={css.overlay} onClick={onClose}>
       <div className={css.modal} onClick={e => e.stopPropagation()}>
         <div className={css.wrapper}>
           <div className={css.text}>
-            <h3>Log out</h3>
+            <div className={css.wrapperBtnClose}>
+              <h3>Log out</h3>
+              <button
+                className={css.closeButton}
+                type="button"
+                onClick={onClose}
+              >
+                <svg width="24" height="24">
+                  <use href="/icons/icons-sprite.svg#close-icon"></use>
+                </svg>
+              </button>
+            </div>
             <p>Do you really want to leave?</p>
           </div>
-          <button type="button" onClick={onClose}>
-            <svg width="24" height="24">
-              <use href="/icons/icons-sprite.svg#close-icon"></use>
-            </svg>
-          </button>
         </div>
+
         <div className={css.control}>
           <button className={css.btnCancel} type="button" onClick={onClose}>
             Cancel
